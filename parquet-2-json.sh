52c0da85daf6ef9cc7ef2e6d9bb557083e89192c
Ryan Williams <ryan@runsascoded.com>
2023-12-07 12:25:27 -0500

    jq, parquet2json helpers

--- parquet-2-json.sh
+++ parquet-2-json.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
+#
+# Wrapper for parquet2json rearranges the arguments:
+#
+# $ parquet2json.sh <subcommand> [args...] <path>
+#
+# as opposed to the original, whose usage says:
+#
+# $ parquet2json [OPTIONS] <FILE> <SUBCOMMAND>
+#
+# but in reality requires:
+#
+# $ parquet2json <FILE> <SUBCOMMAND> [OPTIONS]
+#
+# EIther way, putting the arg last, and the cmd and its options first, makes lots of other piping and wrapping easier.
 
-cmd="$1"; shift
-path="$1"; shift
+last="${@:(($#))}"  # path
+set -- "${@:1:$(($#-1))}"
 
-parquet2json "$path" "$cmd" "$@"
+parquet2json "$last" "$@"
